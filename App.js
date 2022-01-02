b3953400860f19b2586ffc56468db8d2f8db5bbb
KubaM-97 <jakubm16@interia.pl>
2022-01-02 09:02:47 +0100

    nav

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, SafeAreaView, View, ImageBackground, Text } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, SafeAreaView, View, ImageBackground, Text, Modal } from 'react-native';
 import I18n from './lang/i18n';
 import { StatusBar } from 'expo-status-bar';
 import AppLoading from 'expo-app-loading';
@@ -12,17 +12,34 @@ import Main from "~/components/configurator/Main"
 import FinalPanel from "~/components/configurator/final/FinalPanel"
 import Nav from "~/components/global/Nav"
 
+// import Navigator from '~/routes/homeStack'
 const Background = require("~/assets/images/background.jpg")
 
 export default function App() {
+  onBackButtonPress = ()=>console.log('ppp') 
   // onPress: () => {
   //   //     I18n.locale = 'fr-Us';
   //   //     this.setState({changeLanguage: 'English'});
   //   //   },
+  // onPress={pressHandler}
+  const pressHandler = () => {
+    // props.navigation.navigate('ReviewDetails')
+    // props.navigation.navigate('ReviewDetails', {name: 'Dupa', age: 24)
+    props.navigation.push('ReviewDetails')
+    // props.navigation.goBack()
+
+    // props.navigation.getParam('name')
+  }
   let [fontsLoaded] = useFonts({
     'Goldman-Regular':require('~/assets/fonts/Goldman-Regular.ttf'),
   });
 
+  const [xxx, setXXX] = useState(true)
+  const zzz = () => {
+    console.log('pppppppsdsdfsdfsdfsdsdf2cipcipdupduopdpudupdupdidadudaz');
+    setXXX(false)
+  }
+  
   if (!fontsLoaded) return  (
     <Provider store={Store}>
       <AppLoading />
@@ -31,6 +48,11 @@ export default function App() {
   else {
     return (
       <Provider store={Store}>
+        {/* <Modal visible={xxx} onRequestClose={zzz}>
+          <View style={{backgroundColor: 'pink', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>fffffffffffffffffffffFFFFFFFFFFFFFFFFFFffffffffffffffffffff</Text>
+          </View>
+        </Modal> */}
         <ImageBackground source={Background} style={styles.imgBackground}>
           <SafeAreaView style={styles.container}>
             <Nav />
